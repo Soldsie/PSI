412981872801c78004e66f840ce770a33112a6b4
Cheng Han Lee <cheng@soldsie.com>
2015-04-02 12:49:12 -0700

    added shop links

--- src/controller/rank-api-controller.js
+++ src/controller/rank-api-controller.js
@@ -6,7 +6,13 @@ var accountant = require('../service/accountant.js')
 var rankByCategory = function rankByCategory(req, res) {
     var category = req.params.category;
 
-    var brands = ['Michael Kors', 'Coach', 'Louis Vuitton', 'Prada', 'Hermes', 'Gucci'];
+    var brands = [  {name: 'Michael Kors', url:'http://www.michaelkors.com/bags/shoulder-bags/_/N-283l'}, 
+                    {name: 'Coach', url:'http://www.coach.com/shop/women-handbags-shoulder-bags?spu=0&cid=S_G253362'}, 
+                    {name: 'Louis Vuitton', url:'http://us.louisvuitton.com/eng-us/women/handbags'}, 
+                    {name: 'Prada', url:'http://www.prada.com/en/US/e-store/department/woman/handbags.html'}, 
+                    {name: 'Hermes', url:'http://usa.hermes.com/leather/bags-and-luggage/for-day.html'}, 
+                    {name: 'Gucci', url:'http://www.gucci.com/us/category/f/handbags'}
+                ];
 
     async.waterfall(
         [
@@ -19,14 +25,15 @@ var rankByCategory = function rankByCategory(req, res) {
             },
             function(brandsData, waterfallCallback) {
                 var brandDataWithName = _.map(brands, function(brand) {
-                    var brandName = brand.replace(/ /g, '');
+                    var brandName = brand.name.replace(/ /g, '');
 
                     for(var i=0; i<brandsData.length; i++) {
                         var brandData = brandsData[i];
                         var re = new RegExp(brandName, 'i');
                         var hashTag = brandData._id;
                         if(re.test(hashTag)) {
-                            brandData['brandName'] =  brand;
+                            brandData['brandName'] =  brand.name;
+                            brandData['url'] = brand.url;
                             return brandData;
                         } 
                     }
@@ -46,7 +53,8 @@ var rankByCategory = function rankByCategory(req, res) {
                         brandName: brand,
                         likes: randomLikes,
                         comments: randomComments,
-                        score: randomLikes + randomComments * 2
+                        score: randomLikes + randomComments * 2,
+                        url: data.url
                     }
                 });
                 process.nextTick(function() {

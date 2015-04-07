eb115c43446ea9dffd9ee7fac19b118506b1d217
Cheng Han Lee <cheng@soldsie.com>
2015-04-07 11:56:36 -0700

    sort by attribute

--- public/script/rank/rank-view.js
+++ public/script/rank/rank-view.js
@@ -43,11 +43,51 @@ window.psi.view.RankItemView = Backbone.Epoxy.View.extend({
 });
 
 window.psi.view.RankView = Backbone.Epoxy.View.extend({
+    events: {
+        'click .breakdown-stat.comments': 'orderByComments',
+        'click .breakdown-stat.likes': 'orderByLikes',
+        'click .breakdown-stat.tweets': 'orderByTweets',
+        'click .breakdown-stat.retweets': 'orderByRetweets',
+    },
+
     initialize: function(options) {
         this.model = options.model;
         this.el = options.el;
         this.$rankTemplate = options.rankTemplate;
+        this.views = [];
         this.render();
+    },
+
+    orderByComments: function() {
+        this.orderBy('comments');
+    },
+
+    orderByLikes: function() {
+        this.orderBy('likes');
+    },
+
+    orderByTweets: function() {
+        this.orderBy('tweets');
+    },
+
+    orderByRetweets: function() {
+        this.orderBy('retweets');
+    },
+
+    orderBy: function(attribute) {
+        console.log(this.views);
+        var orderedViews = this.views.sort(function(a, b) {
+            return b.model.get(attribute) - a.model.get(attribute);
+        });
+        this.reRender(orderedViews);
+    },
+
+    reRender: function(views) {
+        this.$el = this.el;
+        for(var i=0; i<views.length; i++) {
+            var rankItemView = views[i];
+            this.$el.append(rankItemView.render().el);
+        }
     },
 
     render: function() {
@@ -57,6 +97,7 @@ window.psi.view.RankView = Backbone.Epoxy.View.extend({
                 model: new window.psi.model.RankItem(rankItem),
                 template: _self.$rankTemplate
             });
+            _self.views.push(rankItemView);
             _self.$el.append(rankItemView.render().el);
         });
         return this;
